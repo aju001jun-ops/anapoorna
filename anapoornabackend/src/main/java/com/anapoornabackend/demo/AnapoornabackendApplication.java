774f29522aea290cecb5ef69f8b04975cf234d84
aju001jun-ops <aju001jun@gmail.com>
2026-07-08 19:24:36 +0530

    this is my first commit (basic files)

---
+++ anapoornabackend/src/main/java/com/anapoornabackend/demo/AnapoornabackendApplication.java
@@ -0,0 +1,13 @@
+package com.anapoornabackend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnapoornabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnapoornabackendApplication.class, args);
+	}
+
+}
---
+++ anapoornabackend/src/test/java/com/anapoornabackend/demo/AnapoornabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anapoornabackend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnapoornabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
